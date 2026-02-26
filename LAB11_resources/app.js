7ade815af0f499a78f7396b427feff6c62f76051
BlackShima <Blackshima02@gmail.com>
2026-02-26 10:36:22 +0700

    database

--- LAB11_resources/app.js
+++ LAB11_resources/app.js
@@ -1,18 +1,54 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
+const mysql = require("./config/db");
+const listItem = require("./model/listItem");
+const session = require("express-session");
+const mysqlStore = require("express-mysql-session")(session);
+const Authen = require("./control/authen");
+const UserDB = require("./model/userModel");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
+  listItem.defineInitialItems();
+  const items = await listItem.getTodoItemsName();
   res.render("list", 
-  { listTitle: "Today", newListItems: [] });
+  { listTitle: "Today", newListItems: items });
 });
 
+app.post("/", async function(req, res) {
+    const itemName = req.body.newItem;
+    const newItem = {
+        name: itemName
+    };
+
+    try {
+        await listItem.create(newItem);
+        res.redirect("/");
+    } catch (err) {
+        console.error("Error adding item:", err);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.post("/delete", async function(req, res) {
+    const deleteItemId = req.body.checkbox; 
+    console.log("Deleting Item ID:", deleteItemId);
+    try {
+        const result = await listItem.delete(deleteItemId);
+        if (result) {
+            console.log("Successfully deleted item.");
+            res.redirect("/"); 
+        }
+    } catch (err) {
+        console.error("Error deleting item:", err);
+        res.status(500).send("Server Error");
+    }
+});
 
 app.listen("3000", () => {
   console.log("Server is running on Port 3000.");
--- LAB11_resources/config/db.js
+++ LAB11_resources/config/db.js
@@ -1,18 +1,17 @@
+const { connect } = require("http2");
 const mysql = require("mysql2");
 
 let config =  { 
     host: "localhost",
-    user: "root",
-    password: "root",
-    database: "todolisDB"
-
-
-
-    
+    user: "student",
+    password: "blackshima-01",
+    database: "todolistDB"  
 };
 
+connection = mysql.createConnection(config).promise();
 
-
+connection.query("SELECT 1")
+.then(() => console.log("Connected to MySQL database"))
+.catch((err) => console.error("Error connecting to MySQL database:", err)); 
 
 module.exports = mysql.createConnection(config);
-
